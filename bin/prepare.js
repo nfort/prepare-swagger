79b2efa3dd623aa29e0964f66caf016f85f7d2e3
Kirill Malyakin <kirillmalyakin@MacBook-Air-Kirill.local>
2022-06-16 10:34:23 +0300

    refactor: форматирование и перемеинованние переменных, перенос точки входа

--- bin/prepare.js
+++ bin/prepare.js
@@ -5,12 +5,12 @@ const yaml = require("js-yaml");
 const pipe = require("lodash/fp/pipe");
 const axios = require("axios");
 
-const addMetainfo = require("./lib/transform/addMetainfo");
-const removeDuplicates = require("./lib/transform/removeDuplicates");
-const changeNameOperationId = require("./lib/transform/changeNameOperationId");
-const removeDuplicatesEndpoint = require("./lib/transform/removeDuplicatesEndpoint");
-const allDefinitionRequired = require("./lib/transform/allDefinitionRequired");
-const removeQueryParameters = require("./lib/transform/removeQueryParameters");
+const addMetainfo = require("../lib/transform/addMetainfo");
+const removeDuplicates = require("../lib/transform/removeDuplicates");
+const changeNameOperationId = require("../lib/transform/changeNameOperationId");
+const removeDuplicatesEndpoint = require("../lib/transform/removeDuplicatesEndpoint");
+const allDefinitionRequired = require("../lib/transform/allDefinitionRequired");
+const removeQueryParameters = require("../lib/transform/removeQueryParameters");
 
 const args = process.argv.splice(2);
 const url = args[0];
